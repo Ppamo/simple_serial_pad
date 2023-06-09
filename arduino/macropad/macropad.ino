61ec3137906313903476c79dde0d5c180da09bea
Ppamo <pablo@ppamo.cl>
2023-06-09 00:21:57 -0400

    Complete first version, with multiled color as layers identification

--- arduino/macropad/macropad.ino
+++ arduino/macropad/macropad.ino
@@ -16,6 +16,7 @@
 
 unsigned char led_on = 1;
 unsigned char led_blinking = 0;
+unsigned char layer = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -35,7 +36,6 @@ void setup() {
   digitalWrite(B02, HIGH);
   digitalWrite(B03, HIGH);
   digitalWrite(B04, HIGH);
-  analogWrite(LEDP, 0);
 }
 
 void checkButton(unsigned char button, unsigned char &state){
@@ -43,58 +43,94 @@ void checkButton(unsigned char button, unsigned char &state){
   if (digitalRead(button) == HIGH){
     if (state != 1) {
       state = 1;
-      changed(button, 1);
       if (button == B0S) {
         stateHigmMillis = millis();
         led_blinking = 1;
+      } else {
+        changed(button, 1);
       }
     }
   } else {
     if (state != 0){
       state = 0;
-      changed(button, 0);
       if (button == B0S) {
         stateHigmMillis = 0;
         led_blinking = 0;
+        layer = layer + 1;
+      } else {
+        changed(button, 0);
       }
     }
   }
   if (button == B0S) {
     if (stateHigmMillis > 0){
       if ((millis() - stateHigmMillis) > RESET_INTERVAL){
-        Serial.println("Reset state");
         stateHigmMillis = 0;
         led_blinking = 0;
         led_on = 1;
+        layer = -1;
       }
     }
   }
 }
 
-void controlLed(unsigned char &brightness){
-  if (brightness <= MAX_BRIGHTNESS) {
-    brightness *= BRIGHTNESS_RATE;
-  } else {
-    brightness = MIN_BRIGHTNESS;
-  }
+void writeColor(unsigned char red, unsigned char green, unsigned char blue){
+  analogWrite(LEDR, red);
+  analogWrite(LEDG, green);
+  analogWrite(LEDB, blue);
+}
 
+void writeColorToLed(unsigned char layer, unsigned char brightness){
+  if (brightness == 0) {
+    analogWrite(LEDP, 0);
+  } else {
+    analogWrite(LEDP, 255);
+  }
+  switch (layer) {
+    case 0: // white
+      writeColor(10, 160, 200);
+      break;
+    case 1: // red
+      writeColor(60, 255, 255);
+      break;
+    case 2: // green
+      writeColor(255, 90, 255);
+      break;
+    case 3: // blue
+      writeColor(255, 255, 100);
+      break;
+    case 4: // yellow
+      writeColor(10, 140, 255);
+      break;
+    case 5: // purple
+      writeColor(10, 255, 140);
+      break;
+    case 6: // cyan
+      writeColor(255, 10, 80);
+      break;
+  }
+}
+
+void controlLed(unsigned char &layer, unsigned char &brightness){
   if (led_blinking == 1){
     if (led_on == 1) {
-      analogWrite(LEDR, brightness);
+      writeColorToLed(layer, brightness);
       led_on = 0;
     } else {
-      analogWrite(LEDR, 0);
+      writeColorToLed(layer, 0);
       led_on = 1;
     }
   } else {
-      analogWrite(LEDR, brightness);
+      writeColorToLed(layer, brightness);
   }
   // Serial.println(brightness);
 }
 
 void changed(unsigned char button, unsigned char state){
-  Serial.print(button);
-  Serial.print(" ");
+  Serial.print(layer);
+  Serial.print(";");
+  Serial.print(button - 4);
+  Serial.print(";");
   Serial.println(state);
 }
 
@@ -106,7 +142,7 @@ void loop() {
   static unsigned char stateB04 = 0;
   static unsigned char brightness = MIN_BRIGHTNESS;
   checkButton(B0S, stateB0S);
-  controlLed(brightness);
+  controlLed(layer, brightness);
   checkButton(B01, stateB01);
   checkButton(B02, stateB02);
   checkButton(B03, stateB03);
